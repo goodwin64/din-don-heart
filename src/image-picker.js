93468891d1c429c25f1b8b9b4acd817c0d076f54
Max Donchenko <wereya2@gmail.com>
2018-03-30 22:40:08 +0300

    tech: ESLint ignore pre-defined CRA files + set envs

--- src/image-picker.js
+++ src/image-picker.js
@@ -3,15 +3,15 @@ import workerScript from './worker';
 const myWorker = new Worker(workerScript);
 
 export default function initImagePicker(outCanvas, filepicker) {
-  /* eslint-disable no-param-reassign */
   myWorker.onmessage = (e) => {
     const response = e.data;
+    /* eslint-disable no-param-reassign */
     outCanvas.width = response.width;
     outCanvas.height = response.height;
+    /* eslint-enable no-param-reassign */
     const ctx = outCanvas.getContext('2d');
     ctx.drawImage(response, 0, 0);
   };
-  /* eslint-enable no-param-reassign */
 
   filepicker.addEventListener('change', () => {
     myWorker.postMessage({ file: filepicker.files[0] });
