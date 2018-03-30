c90c78e413864e2818e0d7bb72b8995958ca619d
Max Donchenko <wereya2@gmail.com>
2018-03-30 23:19:26 +0300

    fix: replace local variable creation by using "self" context

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,23 @@
 const workercode = () => {
-  // eslint-disable-next-line
-    let onmessage = e => { // workaround not to use global "self"
+  /* eslint-disable no-restricted-globals */
+  self.onmessage = (e) => {
     createImageBitmap(e.data.file)
       .then((response) => {
-        postMessage(response);
+        self.postMessage(response);
       });
   };
+  /* eslint-enable no-restricted-globals */
 };
 
 let code = workercode.toString();
 code = code.substring(code.indexOf('{') + 1, code.lastIndexOf('}'));
+
+if (!code) {
+  /* eslint-disable no-console */
+  // TODO: throw an error with handling on UI
+  console.error('Image picker Worker does nothing!');
+  /* eslint-enable no-console */
+}
 
 const blob = new Blob([code], { type: 'application/javascript' });
 const workerScript = URL.createObjectURL(blob);
