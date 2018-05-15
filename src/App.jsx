f14a098d5151946664df9630202b96e42d5b0037
Max Donchenko <wereya2@gmail.com>
2018-05-16 00:18:11 +0300

    tech: connect image processing to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import logo from './logo.svg';
 import './App.css';
 
+const defaultFile = {};
+
 export class App extends React.PureComponent {
+  static propTypes = {
+    imageParsingWorker: PropTypes.shape({
+      postMessage: PropTypes.func,
+    }).isRequired,
+  };
+
+  onFileChange = (e) => {
+    const file = e.target.files[0] || defaultFile;
+    this.props.imageParsingWorker.postMessage({ file });
+  };
+
   render() {
     return (
       <div className="App">
@@ -14,7 +28,7 @@ export class App extends React.PureComponent {
         <p className="motto">
           Everything you should know about your heart
         </p>
-        <input type="file" id="filepicker" />
+        <input type="file" id="filepicker" onChange={this.onFileChange} />
         <canvas id="outCanvas" />
       </div>
     );
