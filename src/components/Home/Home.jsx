d0edecc19585d2afc725afa5ca4be4d512c0abc2
Max Donchenko <wereya2@gmail.com>
2018-06-22 06:41:58 +0300

    feat: use image picker in admin section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,9 @@ import {
   imageParsingWorkerPT,
   areEcgExamplesVisiblePT,
   setEcgResultVisibilityPT,
-  onDiseaseResultLocalAnalysisPT, userPT,
+  onDiseaseResultLocalAnalysisPT,
+  userPT,
+  cellsSizePT,
 } from '../../helpers/proptypes.helper';
 import { AppDescription, HomeContainer } from './Home.styled';
 
@@ -34,6 +36,7 @@ import {
 export class App extends Component {
   static propTypes = {
     user: userPT.isRequired,
+    cellsSize: cellsSizePT.isRequired,
     ecgLetters: ecgLettersPT.isRequired,
     ecgLettersDetailed: ecgLettersPT.isRequired,
     localization: localizationPT.isRequired,
@@ -78,14 +81,6 @@ export class App extends Component {
 
     const isAdmin = user.type === 'admin';
 
-    if (isAdmin) {
-      return (
-        <HomeContainer>
-          <DiseaseFormUploader />
-        </HomeContainer>
-      );
-    }
-
     return (
       <HomeContainer>
         <LanguageSelector />
@@ -93,9 +88,15 @@ export class App extends Component {
         <FilePickerConnected />
         {areEcgExamplesVisible && <EcgAnalysisExample />}
         {isEcgResultVisible && <EcgResults />}
-        {ecgLetters.length > 0 && (
+        {ecgLetters.length > 0 && !isAdmin && (
           <DiseaseDetectorConnected
             ecgLettersDetailed={ecgLettersDetailed}
+          />
+        )}
+        {isAdmin && (
+          <DiseaseFormUploader
+            cellsSize={this.props.cellsSize}
+            source={this.props.ecgLettersDetailed}
           />
         )}
       </HomeContainer>
@@ -103,70 +104,114 @@ export class App extends Component {
   }
 }
 
-function DiseaseFormUploader() {
-  return (
-    <div className="col-md-4">
-      <form action="http://176.38.3.120/EKGSample/Add" method="POST">
-        <div className="form-group">
-          <label className="control-label" htmlFor="Mark">Mark</label>
-          <input className="form-control" type="text" id="Mark" name="Mark" defaultValue="" />
-          <span
-            className="text-danger field-validation-valid"
-            data-valmsg-for="Mark"
-            data-valmsg-replace="true"
-          />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="Source">Source</label>
-          <input className="form-control" type="text" id="Source" name="Source" defaultValue="" />
-          <span
-            className="text-danger field-validation-valid"
-            data-valmsg-for="Source"
-            data-valmsg-replace="true"
-          />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="Description">Description</label>
-          <input className="form-control" type="text" id="Description" name="Description" defaultValue="" />
-          <span
-            className="text-danger field-validation-valid"
-            data-valmsg-for="Description"
-            data-valmsg-replace="true"
-          />
-        </div>
-        <div className="form-group">
-          <label className="control-label" htmlFor="cellsSize">cellsSize</label>
+// eslint-disable-next-line react/no-multi-comp
+class DiseaseFormUploader extends React.Component {
+  static propTypes = {
+    cellsSize: cellsSizePT,
+    source: ecgLettersPT,
+  };
+  static defaultProps = {
+    cellsSize: 0,
+    source: '',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cellsSize: props.cellsSize,
+      source: props.source,
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      cellsSize: props.cellsSize,
+      source: props.source,
+    });
+  }
+
+  handleChangeCellSize = (e) => {
+    this.setState({ cellsSize: e.target.value });
+  };
+
+  handleChangeSource = (e) => {
+    this.setState({ source: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="col-md-4">
+        <form action="http://176.38.3.120/EKGSample/Add" method="POST">
+          <div className="form-group">
+            <label className="control-label" htmlFor="Mark">Mark</label>
+            <input className="form-control" type="text" id="Mark" name="Mark" defaultValue="" />
+            <span
+              className="text-danger field-validation-valid"
+              data-valmsg-for="Mark"
+              data-valmsg-replace="true"
+            />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="Source">Source</label>
+            <input
+              className="form-control"
+              type="text"
+              id="Source"
+              name="Source"
+              value={this.state.source}
+              onChange={this.handleChangeSource}
+            />
+            <span
+              className="text-danger field-validation-valid"
+              data-valmsg-for="Source"
+              data-valmsg-replace="true"
+            />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="Description">Description</label>
+            <input className="form-control" type="text" id="Description" name="Description" defaultValue="" />
+            <span
+              className="text-danger field-validation-valid"
+              data-valmsg-for="Description"
+              data-valmsg-replace="true"
+            />
+          </div>
+          <div className="form-group">
+            <label className="control-label" htmlFor="cellsSize">cellsSize</label>
+            <input
+              className="form-control"
+              type="number"
+              data-val="true"
+              data-val-required="The cellsSize field is required."
+              id="cellsSize"
+              name="cellsSize"
+              value={this.state.cellsSize}
+              onChange={this.handleChangeCellSize}
+            />
+            <span
+              className="text-danger field-validation-valid"
+              data-valmsg-for="cellsSize"
+              data-valmsg-replace="true"
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" defaultValue="Create" className="btn btn-default" />
+          </div>
           <input
-            className="form-control"
-            type="number"
-            data-val="true"
-            data-val-required="The cellsSize field is required."
-            id="cellsSize"
-            name="cellsSize"
-            defaultValue="0"
+            name="__RequestVerificationToken"
+            type="hidden"
+            defaultValue="CfDJ8DPUy1YBqOdLkjCP8xD9Uz-RJDLBYE9IQn9umxmXz9HNCKDKALpv1ui9bn_KP7otzuU3peCGtpx2Y2rEonWx3mdacIAsxuYyOATqXe1BKeKlcBC417MEuD8IGkfQeG4IcyfwqHqKXKXRFqnALadik_U"
           />
-          <span
-            className="text-danger field-validation-valid"
-            data-valmsg-for="cellsSize"
-            data-valmsg-replace="true"
-          />
-        </div>
-        <div className="form-group">
-          <input type="submit" defaultValue="Create" className="btn btn-default" />
-        </div>
-        <input
-          name="__RequestVerificationToken"
-          type="hidden"
-          defaultValue="CfDJ8DPUy1YBqOdLkjCP8xD9Uz-RJDLBYE9IQn9umxmXz9HNCKDKALpv1ui9bn_KP7otzuU3peCGtpx2Y2rEonWx3mdacIAsxuYyOATqXe1BKeKlcBC417MEuD8IGkfQeG4IcyfwqHqKXKXRFqnALadik_U"
-        />
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
   return {
     user: state.user,
+    cellsSize: state.ecgResult.cellsSize,
     ecgLetters: state.ecgResult.ecgLetters,
     ecgLettersDetailed: state.ecgResult.ecgLettersDetailed,
     localization: state.appCommonParams.localization,
